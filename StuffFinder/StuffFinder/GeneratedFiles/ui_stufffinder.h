8fdb012d4dfbfd8e8fa55044e0765a9adcce2255
Noelle Todd <decorationgirl@gmail.com>
2014-10-17 14:01:40 -0400

    changed "Add Item" tab; testing signals

--- StuffFinder/StuffFinder/GeneratedFiles/ui_stufffinder.h
+++ StuffFinder/StuffFinder/GeneratedFiles/ui_stufffinder.h
@@ -13,6 +13,8 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
 #include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
@@ -25,6 +27,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTabWidget>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
@@ -40,10 +43,23 @@ public:
     QListWidget *listWidget;
     QWidget *tab_2;
     QListView *listView;
+    QLabel *label;
+    QLabel *label_2;
+    QLineEdit *itemName;
+    QLabel *label_3;
+    QTextEdit *itemDescription;
+    QLabel *label_4;
+    QLineEdit *itemQuantity;
+    QComboBox *categoryCombo;
+    QLabel *label_5;
+    QCheckBox *trackCheck;
+    QLabel *label_6;
+    QLineEdit *minQuantity;
+    QLabel *label_7;
+    QLineEdit *itemCost;
     QGraphicsView *graphicsView;
     QLineEdit *lineEdit;
     QPushButton *pushButton;
-    QLabel *label;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QToolBar *mainToolBar;
@@ -73,6 +89,50 @@ public:
         listView = new QListView(tab_2);
         listView->setObjectName(QStringLiteral("listView"));
         listView->setGeometry(QRect(0, 0, 331, 531));
+        label = new QLabel(tab_2);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(20, 10, 71, 16));
+        label_2 = new QLabel(tab_2);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(30, 40, 61, 16));
+        itemName = new QLineEdit(tab_2);
+        itemName->setObjectName(QStringLiteral("itemName"));
+        itemName->setGeometry(QRect(100, 40, 113, 20));
+        label_3 = new QLabel(tab_2);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(30, 110, 61, 16));
+        itemDescription = new QTextEdit(tab_2);
+        itemDescription->setObjectName(QStringLiteral("itemDescription"));
+        itemDescription->setGeometry(QRect(100, 110, 191, 71));
+        label_4 = new QLabel(tab_2);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(30, 200, 51, 16));
+        itemQuantity = new QLineEdit(tab_2);
+        itemQuantity->setObjectName(QStringLiteral("itemQuantity"));
+        itemQuantity->setGeometry(QRect(90, 200, 41, 20));
+        categoryCombo = new QComboBox(tab_2);
+        categoryCombo->setObjectName(QStringLiteral("categoryCombo"));
+        categoryCombo->setGeometry(QRect(100, 70, 131, 22));
+        label_5 = new QLabel(tab_2);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(30, 70, 51, 16));
+        trackCheck = new QCheckBox(tab_2);
+        trackCheck->setObjectName(QStringLiteral("trackCheck"));
+        trackCheck->setGeometry(QRect(160, 200, 131, 17));
+        label_6 = new QLabel(tab_2);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setEnabled(true);
+        label_6->setGeometry(QRect(30, 240, 101, 16));
+        label_6->setTextInteractionFlags(Qt::LinksAccessibleByMouse);
+        minQuantity = new QLineEdit(tab_2);
+        minQuantity->setObjectName(QStringLiteral("minQuantity"));
+        minQuantity->setGeometry(QRect(130, 240, 41, 20));
+        label_7 = new QLabel(tab_2);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(40, 280, 91, 16));
+        itemCost = new QLineEdit(tab_2);
+        itemCost->setObjectName(QStringLiteral("itemCost"));
+        itemCost->setGeometry(QRect(130, 280, 71, 20));
         tabWidget->addTab(tab_2, QString());
         graphicsView = new QGraphicsView(centralWidget);
         graphicsView->setObjectName(QStringLiteral("graphicsView"));
@@ -83,17 +143,12 @@ public:
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
         pushButton->setGeometry(QRect(960, 40, 75, 23));
-        label = new QLabel(centralWidget);
-        label->setObjectName(QStringLiteral("label"));
-        label->setEnabled(true);
-        label->setGeometry(QRect(490, 40, 121, 16));
         StuffFinderClass->setCentralWidget(centralWidget);
         tabWidget->raise();
         graphicsView->raise();
         Create_db->raise();
         lineEdit->raise();
         pushButton->raise();
-        label->raise();
         menuBar = new QMenuBar(StuffFinderClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 1206, 21));
@@ -110,9 +165,13 @@ public:
         menuBar->addAction(menuFile->menuAction());
 
         retranslateUi(StuffFinderClass);
-        QObject::connect(pushButton, SIGNAL(released()), label, SLOT(show()));
+        QObject::connect(trackCheck, SIGNAL(clicked()), label_7, SLOT(show()));
+        QObject::connect(trackCheck, SIGNAL(clicked()), label_6, SLOT(show()));
+        QObject::connect(trackCheck, SIGNAL(clicked()), minQuantity, SLOT(show()));
+        QObject::connect(trackCheck, SIGNAL(clicked()), itemCost, SLOT(show()));
+        QObject::connect(listView, SIGNAL(viewportEntered()), label_6, SLOT(hide()));
 
-        tabWidget->setCurrentIndex(0);
+        tabWidget->setCurrentIndex(1);
 
 
         QMetaObject::connectSlotsByName(StuffFinderClass);
@@ -123,9 +182,16 @@ public:
         StuffFinderClass->setWindowTitle(QApplication::translate("StuffFinderClass", "StuffFinder", 0));
         Create_db->setText(QApplication::translate("StuffFinderClass", "Create DB", 0));
         tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("StuffFinderClass", "List View", 0));
+        label->setText(QApplication::translate("StuffFinderClass", "Add/Edit Item", 0));
+        label_2->setText(QApplication::translate("StuffFinderClass", "Item Name:", 0));
+        label_3->setText(QApplication::translate("StuffFinderClass", "Description:", 0));
+        label_4->setText(QApplication::translate("StuffFinderClass", "Quantity:", 0));
+        label_5->setText(QApplication::translate("StuffFinderClass", "Category:", 0));
+        trackCheck->setText(QApplication::translate("StuffFinderClass", "Track for shopping list", 0));
+        label_6->setText(QApplication::translate("StuffFinderClass", "Minimum Quantity:", 0));
+        label_7->setText(QApplication::translate("StuffFinderClass", "Estimated Cost:", 0));
         tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("StuffFinderClass", "Add Item", 0));
         pushButton->setText(QApplication::translate("StuffFinderClass", "Search", 0));
-        label->setText(QApplication::translate("StuffFinderClass", "Hullo World", 0));
         menuFile->setTitle(QApplication::translate("StuffFinderClass", "File", 0));
     } // retranslateUi
 
