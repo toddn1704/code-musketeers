93cf78a19d7175a26261d43c4d1a48fba01f5d32
rpikeaton <keaton@rpi.edu>
2014-10-17 13:02:57 -0400

    Updated category.

--- StuffFinder/StuffFinder/Category.h
+++ StuffFinder/StuffFinder/Category.h
@@ -7,10 +7,15 @@
 class Category {
 public:
 	Category();
+	Category(std::string n,std::string d) : name(n),description(d) {}
+
 	void set_name(std::string n) { name = n; }
 	void set_description(std::string d) { description = d; }
+	void add_item(Item* i) { items.push_back(i); }
+
 	std::string get_name() { return name; }
 	std::string get_description() { return description; }
+	std::vector<Items*> get_items() { return items; }
 	
 
 private:
