4b544b179a138f9e8153b75888ab118fea24ea76
huj10 <huj10@rpi.edu>
2014-10-22 16:09:56 -0400

    Changed category vector to map

--- StuffFinder/StuffFinder/Category.h
+++ StuffFinder/StuffFinder/Category.h
@@ -12,18 +12,18 @@ public:
 	void set_name(std::string n) { name = n; }
 	void set_description(std::string d) { description = d; }
 	void set_category_id(int i) { category_id = i; }
-	void add_item(Item* i) { items.push_back(i); }
+	void add_item(Item* i) { items.insert(std::make_pair(i->get_name(),i)); }
 
 	int get_category_id() { return category_id; }
 	std::string get_name() { return name; }
 	std::string get_description() { return description; }
-	std::vector<Item*> get_items() { return items; }
+	std::map<std::string, Item*> get_items() { return items; }
 	
 
 private:
 	int category_id;
 	std::string name;
 	std::string description;
-	std::vector<Item*> items;
+	std::map<std::string, Item*> items;
 };
 #endif
